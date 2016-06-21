e6440313d14f26f513fd0b90a3650ee5d9b57f3c
spanky <spanky@spankys-MBP.lan>
2016-06-21 10:21:14 -0400

    Initial commit, works given two environment variables pointing to the download directory and the config file

--- get.py
+++ get.py
@@ -7,12 +7,30 @@ import time
 # TODO: Run in loop and check exit code status, run again if error
 # TODO: Run periodically to check updates in the downloadfile
 # TODO: Remove entries from the downloadfile as the thing completes
+#   * Accomplish this by reading config chunks out of the file and writing the remainder
+
+# TODO: Change config file format to something like
+"""
+[Playlist Title]
+url=http://youtube.com/some_video_url
+sync=True (defaults to False)
+audio=False (defaults to False)
+"""
+# Where sync will keep it in the queue file to be re-downloaded periodically
+# And audio will only retain the audio file
 
 # TODO: Make this work on Windows?
-#DOWNLOADFILE = os.path.join(os.environ.get('HOME'), 'Dropbox','youtubedl.txt')
-DOWNLOADFILE = os.path.join("c:\\Users\\spanky\\Dropbox\\youtubedl.txt")
-#MOVIEDIR = '/Users/spanky/Movies'
-MOVIEDIR = 'm:\\Youtube'
+try:
+    DOWNLOADFILE = os.environ['DOWNLOADFILE']
+except KeyError:
+    print("You must set the environment variable DOWNLOADFILE to point to your list of URLs!")
+    sys.exit(1)
+
+try:
+    DLDIR = os.environ['DLDIR']
+except KeyError:
+    print("You must set the environment variable DLDIR to point to your download directory!!")
+    sys.exit(1)
 
 def getArglist(url):
     """
@@ -31,19 +49,29 @@ def getArglist(url):
 
     return args
 
+def getPlaylistData(fp):
+    # Returns a name the the player and the URL, given a file pointer to a playlist list.
+    print("Reading from file...")
+    name = f.readline().rstrip()
+    url = f.readline().rstrip()
+    print("Name is: %s" % name)
+    print("URL is: %s" % url)
+    return name, url
+
+
 if __name__ == "__main__":
     print("Checking queuefile %s for updates..." % DOWNLOADFILE)
     with open(DOWNLOADFILE,'r') as f:
         while True:
-            print("Reading from file...")
-            name = f.readline().rstrip()
-            url = f.readline().rstrip()
-            print("Name is: %s" % name)
-            print("URL is: %s" % url)
-            #queuefile = f.read()
+            name, url = getPlaylistData(f)
+            #print("Reading from file...")
+            #name = f.readline().rstrip()
+            #url = f.readline().rstrip()
+            #print("Name is: %s" % name)
+            #print("URL is: %s" % url)
 
             if name and url:
-                playlistdir = os.path.join(MOVIEDIR, name)
+                playlistdir = os.path.join(DLDIR, name)
                 if not os.path.exists(playlistdir):
                     print("Creating directory %s" % playlistdir)
                     os.mkdir(playlistdir)
@@ -53,11 +81,5 @@ if __name__ == "__main__":
                 p = subprocess.Popen(args=args, cwd=playlistdir)
                 p.wait()
 
-                """
-                if p.returncode == 0:
-                    print("Successfully downloaded %s, removing from queuefile..." % name)
-                    with open(DOWNLOADFILE,'w') as f:
-                        f.write(queuefile)
-                """
-            print("Reached end of loop, sleeping for 10 secs...")
+            print("Reached end of loop, sleeping for 11 secs...")
             time.sleep(10)
