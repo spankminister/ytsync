eacc4ce00582eb4d8b9f757886e4fd5ff3fe8fc1
spanky <spanky@spankys-MBP.home>
2016-06-30 21:13:50 -0400

    Working now with audio extraction and syncing. Need to formalize end-to-end testing next.

--- get.py
+++ get.py
@@ -3,21 +3,14 @@
 import subprocess
 import os
 import time
+import configparser
+
+# TODO: Implement ConfigParser bit
 
 # TODO: Run in loop and check exit code status, run again if error
 # TODO: Run periodically to check updates in the downloadfile
 # TODO: Remove entries from the downloadfile as the thing completes
 #   * Accomplish this by reading config chunks out of the file and writing the remainder
-
-# TODO: Change config file format to something like
-"""
-[Playlist Title]
-url=http://youtube.com/some_video_url
-sync=True (defaults to False)
-audio=False (defaults to False)
-"""
-# Where sync will keep it in the queue file to be re-downloaded periodically
-# And audio will only retain the audio file
 
 # TODO: Make this work on Windows?
 try:
@@ -49,37 +42,47 @@ def getArglist(url):
 
     return args
 
-def getPlaylistData(fp):
-    # Returns a name the the player and the URL, given a file pointer to a playlist list.
-    print("Reading from file...")
-    name = f.readline().rstrip()
-    url = f.readline().rstrip()
-    print("Name is: %s" % name)
-    print("URL is: %s" % url)
-    return name, url
+def downloadPlaylist(cfgsection):
+    """
+    Pass in a config section and actually do the downloading.
+    """
+    name = cfgsection.name
+    url = cfgsection.get('url')
 
+    if name and url:
+        playlistdir = os.path.join(DLDIR, name)
+        if not os.path.exists(playlistdir):
+            print("Creating directory %s" % playlistdir)
+            os.mkdir(playlistdir)
+
+        args = getArglist(url)
+        if cfgsection.getboolean('audio'):
+            args.append('--extract-audio')
+            args.append('--audio-format=mp3')
+            if cfgsection.getboolean('sync'):
+                # If we're going to be repeatedly syncing this playlist
+                # it makes sense to keep the originals around to avoid
+                # re-downloading the whole thing every time.
+                args.append('--keep-video')
+
+        p = subprocess.Popen(args=args, cwd=playlistdir)
+        p.wait()
 
 if __name__ == "__main__":
-    print("Checking queuefile %s for updates..." % DOWNLOADFILE)
-    with open(DOWNLOADFILE,'r') as f:
-        while True:
-            name, url = getPlaylistData(f)
-            #print("Reading from file...")
-            #name = f.readline().rstrip()
-            #url = f.readline().rstrip()
-            #print("Name is: %s" % name)
-            #print("URL is: %s" % url)
+    # This exterior loop should just do the config file reading and rewriting
+    # While dispatching youtube-dl for the actual work
+    #
+    while True:
+        print("Checking queuefile %s for updates..." % DOWNLOADFILE)
+        config = configparser.ConfigParser()
+        config.read(DOWNLOADFILE)
 
-            if name and url:
-                playlistdir = os.path.join(DLDIR, name)
-                if not os.path.exists(playlistdir):
-                    print("Creating directory %s" % playlistdir)
-                    os.mkdir(playlistdir)
+        for plname in config.sections():
+            section = config[plname]
+            downloadPlaylist(section)
+            if not section.getboolean('sync'):
+                pass # TODO: Delete the section from the new config
 
-                args = getArglist(url)
-
-                p = subprocess.Popen(args=args, cwd=playlistdir)
-                p.wait()
-
-            print("Reached end of loop, sleeping for 11 secs...")
-            time.sleep(10)
+            TIMESLEEP_MINUTES = 60
+            print("Reached end of loop, sleeping for %d minutes..." % TIMESLEEP_MINUTES)
+            time.sleep(TIMESLEEP_MINUTES*60)
